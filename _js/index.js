3499ee5030da1be0673243cae3f4d2c8f7df7431
Sam Stephenson <sam@37signals.com>
2018-01-26 17:00:02 -0600

    Add Turbolinks
    Because why not?

--- _js/index.js
+++ _js/index.js
@@ -1,5 +1,8 @@
+import Turbolinks from "turbolinks"
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+Turbolinks.start()
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
