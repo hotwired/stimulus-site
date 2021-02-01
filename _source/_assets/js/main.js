9451b6441b7433a0a9715b5611e644be4b0186cd
Adam Stoddard <adam@adamstoddard.com>
2021-02-01 08:55:29 -0800

    bump

--- _source/_assets/js/main.js
+++ _source/_assets/js/main.js
@@ -1,8 +1,8 @@
 import "@hotwired/turbo"
 import { Application } from "stimulus"
-
 import HelloController from "./controllers/hello_controller"
 import CalloutController from "./controllers/callout_controller"
+
 const application = Application.start()
 application.register("hello", HelloController)
 application.register("callout", CalloutController)
