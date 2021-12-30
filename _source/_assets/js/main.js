a9de6bd16fb490a337e318cde0a5a3514db55019
qichunren <whyruby@gmail.com>
2021-12-30 17:58:22 +0800

    Fixed redirect to youtube link when site page in iframe.

--- _source/_assets/js/main.js
+++ _source/_assets/js/main.js
@@ -6,9 +6,3 @@ import CalloutController from "./controllers/callout_controller"
 const application = Application.start()
 application.register("hello", HelloController)
 application.register("callout", CalloutController)
-
-// Are we in an <iframe>?
-if (window.top !== window) {
-  // 🎶 Never Gonna Give You Up 🎶
-  location.replace("https://www.youtube.com/embed/dQw4w9WgXcQ")
-}
